97aee3236df9b95dbd452282bb998d4d1f7ff7db
Logan Martlew <logan.martlew@gmail.com>
2021-07-22 14:37:28 +1200

    Added react-router with base route and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Layout from './components/layout';
+import HomePage from './components/pages/HomePage';
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <Layout>
+      <Router>
+        <Route path='/' component={HomePage} exact />
+      </Router>
+    </Layout>
+  );
 };
 
 export default App;
