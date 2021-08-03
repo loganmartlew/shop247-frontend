30796e021b47f4b8d37828558a25a8dbfed6c103
Logan Martlew <logan.martlew@gmail.com>
2021-08-03 17:41:44 +1200

    Added an error modal to be used for displaying critical notifications

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,30 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Layout from './components/layout';
 import HomePage from './components/pages/HomePage';
 import ProfilePage from './components/pages/ProfilePage';
+import ErrorProvider from './contexts/ErrorContext';
 
 const App = () => {
   return (
-    <Layout>
-      <Router>
-        <Route path='/' component={HomePage} exact />
+    <ErrorProvider>
+      <Layout>
+        <Router>
+          <Route path='/' component={HomePage} exact />
 
-        <Route path='/search' />
-        <Route path='/products/:productid' />
-        <Route path='/categories' />
-        <Route path='/categories/:category' />
+          <Route path='/search' />
+          <Route path='/products/:productid' />
+          <Route path='/categories' />
+          <Route path='/categories/:category' />
 
-        <Route path='/profile' component={ProfilePage} />
-        <Route path='/profile/orders' />
-        <Route path='/user/:userid' />
+          <Route path='/profile' component={ProfilePage} />
+          <Route path='/profile/orders' />
+          <Route path='/user/:userid' />
 
-        <Route path='/cart' />
-        <Route path='/checkout' />
-        <Route path='/pay' />
-      </Router>
-    </Layout>
+          <Route path='/cart' />
+          <Route path='/checkout' />
+          <Route path='/pay' />
+        </Router>
+      </Layout>
+    </ErrorProvider>
   );
 };
 
