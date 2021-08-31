ef5c545322483ea16616bc55ee12c0da93270cf7
Logan Martlew <logan.martlew@gmail.com>
2021-08-31 16:49:47 +1200

    Removed unused code from profile page and removed ErrorContext

--- src/components/pages/ProfilePage/index.jsx
+++ src/components/pages/ProfilePage/index.jsx
@@ -1,40 +1,7 @@
-import { useNotification } from '../../../contexts/NotificationContext';
-
 const ProfilePage = () => {
-  const { addError, addWarn, addSuccess } = useNotification();
-
-  const getRand = () => (Math.random() * 100).toFixed(0);
-
-  const error = e => {
-    e.preventDefault();
-
-    const rand = getRand();
-
-    addError('Error ' + rand);
-  };
-
-  const warn = e => {
-    e.preventDefault();
-
-    const rand = getRand();
-
-    addWarn('Warn ' + rand);
-  };
-
-  const success = e => {
-    e.preventDefault();
-
-    const rand = getRand();
-
-    addSuccess('Success ' + rand);
-  };
-
   return (
     <>
       <h1>Profile</h1>
-      <button onClick={error}>Error</button>
-      <button onClick={warn}>Warn</button>
-      <button onClick={success}>Success</button>
     </>
   );
 };
