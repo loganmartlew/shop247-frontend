d0b14790c8a53a267e89d3fc672682108cadd9ea
Logan Martlew <logan.martlew@gmail.com>
2021-09-15 23:17:07 +1200

    Prettier formatting

--- src/components/pages/CartPage/index.jsx
+++ src/components/pages/CartPage/index.jsx
@@ -4,32 +4,30 @@ import { List } from '../../products/ProductList/ProductListStyles';
 import SingleProductTile from '../../products/SingleProductTile';
 
 const CartPage = () => {
-    const isEmpty = true;
+  const isEmpty = true;
 
-    const EmptyCart = () => (
-        <h3>Your cart is currently empty!</h3>
-    )
+  const EmptyCart = () => <h3>Your cart is currently empty!</h3>;
 
-    const FilledCart = ({products}) => {
-        <>
-            <List>
-            {products &&
-                products.map(product => (
-                <SingleProductTile key={product._id} product={product} />
-            ))}
-            </List>
-        </>
-    }
+  const FilledCart = ({ products }) => {
+    <>
+      <List>
+        {products &&
+          products.map(product => (
+            <SingleProductTile key={product._id} product={product} />
+          ))}
+      </List>
+    </>;
+  };
 
-    return (
-        <>
-            <CartSection>
-                <h1>Your Cart</h1>
+  return (
+    <>
+      <CartSection>
+        <h1>Your Cart</h1>
 
-                { isEmpty ? <EmptyCart /> : <FilledCart /> }
-            </CartSection>
-        </>
-    )
-}
+        {isEmpty ? <EmptyCart /> : <FilledCart />}
+      </CartSection>
+    </>
+  );
+};
 
 export default CartPage;
