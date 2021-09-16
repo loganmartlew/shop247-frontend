a7120192cd7cd635f5133a2182db104fac91fbea
Logan Martlew <logan.martlew@gmail.com>
2021-09-17 10:47:20 +1200

    Added seller name to product page

---
+++ src/components/pages/ProductPage/ProductPageStyles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const ProductContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  max-width: 700px;
+  margin: 3rem auto;
+  padding: 1.5em;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.2);
+`;
+
+export const Name = styled.h2`
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+export const Description = styled.h3`
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+export const Price = styled.h3`
+  color: ${({ theme }) => theme.colors.black};
+`;
--- src/components/pages/ProductPage/index.jsx
+++ src/components/pages/ProductPage/index.jsx
@@ -1,4 +1,5 @@
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import useSWR from 'swr';
 import { fetchApi } from '../../../util/fetchApi';
 import formatDisplayPrice from '../../../util/formatDisplayPrice';
@@ -7,7 +8,8 @@ import {
   Description,
   Price,
   ProductContainer,
-} from './ProducctPageStyles';
+} from './ProductPageStyles';
+import Button from '../../Button';
 
 const fetcher = (...args) => fetchApi(...args).then(res => res.json());
 
@@ -15,18 +17,36 @@ const ProductPage = () => {
   const { productid } = useParams();
 
   const { data } = useSWR(`/products/${productid}`, fetcher);
+  const [user, setUser] = useState(null);
 
-  const ProductCard = ({ product }) => (
+  useEffect(() => {
+    if (!data?.product) return;
+
+    fetchApi(`/users/${data.product.sellerId}`)
+      .then(res => res.json())
+      .then(data => setUser(data.user));
+  }, [data]);
+
+  const ProductCard = ({ product, user }) => (
     <ProductContainer>
       <Name>{product.name}</Name>
       <Description>{product.description}</Description>
+      <p>Seller: {user.name}</p>
+      <Link to={`/user/${user.uid}`}>
+        <Button>View Profile</Button>
+      </Link>
       <Price>{formatDisplayPrice(product.price)}</Price>
+      {product.images.map(({ url }, i) => (
+        <img src={url} key={i} alt='' />
+      ))}
     </ProductContainer>
   );
 
   return (
     <>
-      {data && data.product && <ProductCard product={data.product} />}
+      {data?.product && user && (
+        <ProductCard product={data.product} user={user} />
+      )}
       {data?.message && <h2>Product Not Found</h2>}
       {!data && <h2>Loading...</h2>}
     </>
