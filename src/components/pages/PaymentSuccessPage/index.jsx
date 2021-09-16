9b46cc6191062e2034e720d48e4017c27acf9578
Logan Martlew <logan.martlew@gmail.com>
2021-09-16 20:27:15 +1200

    Added functionality to clear cart when an order succeeds

--- src/components/pages/PaymentSuccessPage/index.jsx
+++ src/components/pages/PaymentSuccessPage/index.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useCart } from '../../../contexts/CartContext';
 import Button from '../../Button';
 
 const PaymentSuccessPage = () => {
+  const { clearCart } = useCart();
+
+  useEffect(() => {
+    clearCart();
+  }, [clearCart]);
+
   return (
     <div>
       <h1>Thank you for your purchase!</h1>
