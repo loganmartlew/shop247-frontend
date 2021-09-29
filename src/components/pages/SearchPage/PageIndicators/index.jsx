cfa0a2529c85ad2904f809a3e7563b84f4dd231b
Logan Martlew <logan.martlew@gmail.com>
2021-09-29 18:16:43 +1300

    Added left/right navigation to buttons

--- src/components/pages/SearchPage/PageIndicators/index.jsx
+++ src/components/pages/SearchPage/PageIndicators/index.jsx
@@ -1,16 +1,40 @@
+import { useHistory } from 'react-router-dom';
 import populateArrayLength from '../../../../util/populateArrayLength';
 import Indicator from './Indicator';
 
 const PageIndicators = ({ pages }) => {
   const pageArr = populateArrayLength(new Array(pages.total));
 
+  const history = useHistory();
+
+  const getPath = modifier => {
+    const query = new URLSearchParams(history.location.search);
+    query.set('page', pages.page + modifier);
+
+    const path = `${history.location.pathname}?${query.toString()}`;
+
+    return path;
+  };
+
   return (
     <div>
-      <button>&lt;</button>
+      <button
+        disabled={pages.page === 1}
+        onClick={() => history.push(getPath(-1))}
+      >
+        &lt;
+      </button>
+
       {pageArr.map((_, i) => (
         <Indicator page={i + 1} active={pages.page === i + 1} key={i} />
       ))}
-      <button>&gt;</button>
+
+      <button
+        disabled={pages.page === pages.total}
+        onClick={() => history.push(getPath(1))}
+      >
+        &gt;
+      </button>
     </div>
   );
 };
