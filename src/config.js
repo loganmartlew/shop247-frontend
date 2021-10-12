b526cdff8e87760acf59d2cbcb531c35502b7fc5
loganmartlew <40407507+loganmartlew@users.noreply.github.com>
2021-10-13 01:30:04 +1300

    Fixed api config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const apiUrl = 'https://shop247-backend.herokuapp.com';
-export const apiUrl = 'http://localhost:5000';
+export const apiUrl = 'https://shop247-backend.herokuapp.com';
+// export const apiUrl = 'http://localhost:5000';
