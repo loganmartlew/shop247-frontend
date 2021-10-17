6e6124fbde2701656175dfce74dd64e8804757d1
Kyle <oasank@gmail.com>
2021-10-17 19:26:01 +1300

    Prettier formatter

--- src/components/pages/EditProfilePage/index.jsx
+++ src/components/pages/EditProfilePage/index.jsx
@@ -2,9 +2,7 @@ import { useState, useRef, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
-import { 
-    PageWrapper, 
-} from "./EPPStyles";
+import { PageWrapper } from './EPPStyles';
 
 import {
   Form,
@@ -19,58 +17,52 @@ import { useAuth } from '../../../contexts/AuthContext';
 import { useNotification } from '../../../contexts/NotificationContext';
 
 const EditProfilePage = () => {
-    const { user } = useAuth();
-    const { addError, addSuccess } = useNotification();
+  const { user } = useAuth();
+  const { addError, addSuccess } = useNotification();
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({});
 
-    });
+  return (
+    <>
+      <PageWrapper>
+        <Form onSubmit={handleSubmit()}>
+          <h3> Change Account Details </h3>
+          <FormGroup>*** this where ren puts his ting ***</FormGroup>
+          <FormGroup>
+            <FieldLabel htmlFor='name'>User Name:</FieldLabel>
+            <TextInput
+              type='text'
+              id='name'
+              {...register('name', { required: 'Username is required' })}
+            ></TextInput>
+            <ErrorMessage>{ErrorMessage.name?.message}</ErrorMessage>
+          </FormGroup>
+          <FormGroup>
+            <FieldLabel htmlFor='socials'>Social Links:</FieldLabel>
+            <TextInput
+              type='text'
+              id='socials'
+              // {...register('socials', { required: 'Socials is required'})}
+            ></TextInput>
+            <ErrorMessage>{ErrorMessage.socials?.message}</ErrorMessage>
+          </FormGroup>
 
-    return(
-        <>
-            <PageWrapper>
-                <Form onSubmit={handleSubmit()}>
-                    <h3> Change Account Details </h3>
-                    <FormGroup>
-                        *** this where ren puts his ting ***
-                    </FormGroup>
-                    <FormGroup>
-                        <FieldLabel htmlFor='name'>User Name:</FieldLabel>
-                        <TextInput
-                            type='text'
-                            id='name'
-                            {...register('name', { required: 'Username is required'})}
-                        >
-                        </TextInput>
-                        <ErrorMessage>{ErrorMessage.name?.message}</ErrorMessage>
-                    </FormGroup>
-                    <FormGroup>
-                        <FieldLabel htmlFor='socials'>Social Links:</FieldLabel>
-                        <TextInput
-                            type='text'
-                            id='socials'
-                            // {...register('socials', { required: 'Socials is required'})}
-                        >
-                        </TextInput>
-                        <ErrorMessage>{ErrorMessage.name?.message}</ErrorMessage>
-                    </FormGroup>
-
-                    <Button type='submit' solid>
-                        Submit
-                    </Button>
-                    <Button>
-                        <Link to={`/user/${user.uid}`}>Back</Link>
-                    </Button>
-                </Form>
-            </PageWrapper>
-        </>
-    )
-}
+          <Button type='submit' solid>
+            Submit
+          </Button>
+          <Button>
+            <Link to={`/user/${user.uid}`}>Back</Link>
+          </Button>
+        </Form>
+      </PageWrapper>
+    </>
+  );
+};
 
 export default EditProfilePage;
